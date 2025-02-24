29fe1c6b8a8e564f65797989fd321376256dd6b3
danilo.arenasyi <danilo.arenasyi@gmail.com>
2025-02-24 22:01:19 +0000

    feat: dashboard design layout

--- src/app/Layouts/dashboard/dashboard.component.ts
+++ src/app/Layouts/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../Components/Dashboard/navbar/navbar.component';
+import { SidebarComponent } from '../../Components/Dashboard/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
